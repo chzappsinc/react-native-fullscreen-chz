8cb6f602eec5ff59f43bdddbcefd9b27f0986648
Jasim Tk <65512279+chzappsinc@users.noreply.github.com>
2021-07-19 05:16:46 +0530

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+import { NativeModules } from 'react-native';
+
+const { ReactNativeFullscreen } = NativeModules;
+/**
+ * @param enable
+ * @param disable
+ * 
+ * To enable full screen then 
+ * 
+ * ReactNativeFullscreen.enable()
+ * 
+ * To disable full screen 
+ * 
+ * ReactNativeFullscreen.disable()
+ * 
+ * if you use  StatusBar then hide it!
+ * 
+ * **/ 
+export default ReactNativeFullscreen;
+
+
+/**
+ * 
+ 
+▒█▀▀█ ▒█░▒█ ▒█▀▀▀█ ░█▀▀█ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▀█ 
+▒█░░░ ▒█▀▀█ ░▄▄▄▀▀ ▒█▄▄█ ▒█▄▄█ ▒█▄▄█ ░▀▀▀▄▄ 
+▒█▄▄█ ▒█░▒█ ▒█▄▄▄█ ▒█░▒█ ▒█░░░ ▒█░░░ ▒█▄▄▄█
+
+COPYRIGHT CHZAPPS
+ * 
+ * */ 
---
+++ index.js
@@ -0,0 +1,32 @@
+import { NativeModules } from 'react-native';
+
+const { ReactNativeFullscreen } = NativeModules;
+
+/**
+ * @param enable
+ * @param disable
+ * 
+ * To enable full screen then 
+ * 
+ * ReactNativeFullscreen.enable()
+ * 
+ * To disable full screen 
+ * 
+ * ReactNativeFullscreen.disable()
+ * 
+ * if you use  StatusBar then hide it!
+ * 
+ * **/
+export default ReactNativeFullscreen;
+
+
+/**
+ *
+
+▒█▀▀█ ▒█░▒█ ▒█▀▀▀█ ░█▀▀█ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▀█
+▒█░░░ ▒█▀▀█ ░▄▄▄▀▀ ▒█▄▄█ ▒█▄▄█ ▒█▄▄█ ░▀▀▀▄▄
+▒█▄▄█ ▒█░▒█ ▒█▄▄▄█ ▒█░▒█ ▒█░░░ ▒█░░░ ▒█▄▄▄█
+
+COPYRIGHT CHZAPPS
+ *
+ * */
