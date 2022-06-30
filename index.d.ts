26c0b267d975a500bb69a006a18a48cd5da9dbf0
chzappsinc <enteedakkadoffical@gmail.com>
2022-07-01 03:03:16 +0530

    - Fixed all bugs

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,27 @@
 declare module "react-native-fullscreen-chz" {
   export default class FullScreenChz {
     /**
-     * @see
+     * @see https://github.com/chzappsinc/react-native-fullscreen-chz/tree/main/example#enable
      * Enable FullScreen in Android
      * Hide Both StatusBar and NavigationBar
+     *
+     * @author Jasim T K
+     * @see https://chzapps.com
+     *
      */
     static enable(): void;
     /**
-     * Disable fullScreen View in Android
+     * @see https://github.com/chzappsinc/react-native-fullscreen-chz/tree/main/example#disable
+     *
+     * Disable fullScreen View in Android (Animated = false)
+     *
+     * @see https://github.com/chzappsinc/react-native-fullscreen-chz/tree/main/upcoming#animated-visible
+     *
+     * @author Jasim T K
+     * @see https://chzapps.com
+     *
      */
     static disable(): void;
-    /**
-     * Open app with package id
-     */
-    static openApplication(): void;
   }
 }
 
